9c357f2d89e1ca86da8d26381ec308bd83661f3f
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-02-21 15:20:43 +0530

    react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import "./App.css";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 function App() {
   return (
-    <>
-      <p className="font-bold text-red-800">hello App</p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
